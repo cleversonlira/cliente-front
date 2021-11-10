5eafe463c1be702a0f2036c29332cf9aec8fed22
cleversonlira <cleverson_lira@outlook.com>
2021-11-10 15:28:42 -0300

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./interfaces/main/main.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full'},
+  { path: 'main', component: MainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './interfaces/app.component';
+import { MainComponent } from './interfaces/main/main.component';
+import { FormularioComponent } from './interfaces/main/formulario/formulario.component';
+import { CabecalhoComponent } from './interfaces/main/cabecalho/cabecalho.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FormularioComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule,
